fe3eb87df6e6cee1b03157b2c9ebe2a602d60a3e
tm <torrez.mn@gmail.com>
2023-11-29 10:23:41 -0300

    UPDATED - {2023-11-29} - {10:23:40}

--- src/main_global.py
+++ src/main_global.py
@@ -1,59 +1,53 @@
 import feedparser
 from helpers.global_rss import global_feeds
 from helpers.tools import pprint_data, get_file_tree, append_data, wait_random_time
+import json
 
 
-def build_row(name, section, i):
+def build_row(name, section, entry):
     data = {
         "news_paper": name,
         "section": section,
-        "title": i.get("title", ""),
-        "link": i.get("link", ""),
-        "summary": i.get("summary", ""),
-        "published": i.get("published", ""),
-        "tags": i.get("tags", ""),
-        "credit": i.get("credit", ""),
+        "title": entry.get("title", ""),
+        "link": entry.get("link", ""),
+        "summary": entry.get("summary", ""),
+        "published": entry.get("published", ""),
+        "tags": entry.get("tags", ""),
+        "credit": entry.get("credit", ""),
     }
-    my_file = get_file_tree(name, section, "Argentina")
-    print("Adding one.")
-    append_data(str(my_file), data)
+    my_file = get_file_tree(name, section, "World")
+    try:
+        with open(my_file, "a") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
+    except UnicodeEncodeError as e:
+        print(f"Encoding error: {e}")
 
 
 def parse_url(name, section, url):
-    print("Parsing url: ", url)
+    print(f"SECTION: {section}")
+    print(f"NAME: {name}")
     NewsFeed = feedparser.parse(url)
-    print("\n" * 5)
-    print("\n" * 5)
-    print(dict(NewsFeed)["feed"])
-    print("\n" * 5)
+    entries = []
+    for entry in NewsFeed.entries:
+        if "Milei" in entry["title"]:
+            entries.append(entry)
+            break
 
-
+    if entries:
+        for entry in entries:
+            try:
+                build_row(name, section, entry)
+            finally:
+                wait_random_time()
 
 
 if __name__ == "__main__":
     zonas = list(global_feeds.keys())
-    for i in zonas:
-        diarios = global_feeds[i].keys()
-        for j in diarios:
-            sections = global_feeds[i][j].keys()
-            for k in sections:
-                print(f'ZONA -> {i}')
-                print(f'DIARIO -> {j}')
-                print(f'SECTIONS -> {k}')
-                print(f'{k} - {global_feeds[i][j][k]}')
-            break
-        break
+    for zona in zonas:
+        diarios = global_feeds[zona].keys()
+        for diario in diarios:
+            sections = global_feeds[zona][diario].keys()
+            for section in sections:
+                url = global_feeds[zona][diario][section]
+                parse_url(diario, section, url)
 
-
-
-
-
-
-
-   #     wait_random_time()
-   #     for j in secciones:
-   #         url = global_feeds[i][j]
-   #         parse_url(i, j, url)
-   #         #         wait_random_time()
-   #         break
-   #     break
