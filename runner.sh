063e9cfc44c4813572d89c533b138168ddaa7332
tm <torrez.mn@gmail.com>
2024-10-18 13:28:12 -0300

    🚨 🚨 🚨 - Last updated viernes at 01:28 .
    I completely forgot what I did so that's why this message is.

--- runner.sh
+++ runner.sh
@@ -5,3 +5,15 @@ python3 main_argentina.py
 
 echo "Running global."
 python3 main_global.py
+
+
+
+
+# Get current date and time
+current_date=$(date '+%Y-%m-%d')
+current_time=$(date '+%H:%M:%S')
+
+# Append the log message to push_log.txt
+echo "$current_date  $current_time | SE EJECUTO CORRECTAMENTE!" >> /home/torrezmn/Documentos/Milei_News/push_log.txt 
+
+
